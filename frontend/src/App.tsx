4fd2187961e9b17ec3ebe7b0351a8ca93d839f57
Omer <omeralon53@gmail.com>
2022-07-18 18:44:54 +0300

    Fetch the backend data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CoinInfo from "./CoinInfo";
 import "./App.css";
 
 function App()
 {
+  const [backendData, setBackendData] = useState<CoinInfo>({} as CoinInfo);
+
+  useEffect(() =>
+  {
+    fetch("/fetch").then((response) => response.json())
+      .then((data) =>
+      {
+        setBackendData(data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <span className="Heading">
-        Type in the coins you&apos;re interested in
+        Investing Made Easy.
       </span>
     </div>
   );

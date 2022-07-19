9af47f8b1e89fef9cbbcd459235b5b19f95ad3cf
Omer <omeralon53@gmail.com>
2022-07-19 17:47:49 +0300

    Use the created components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from "react";
 import CoinInfo from "./CoinInfo";
 import "./App.css";
+import DisplayGraphs from "./components/DisplayGraphs";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
 
 function App()
 {
   const [backendData, setBackendData] = useState<CoinInfo>({} as CoinInfo);
+
+  Chart.register(CategoryScale);
 
   useEffect(() =>
   {
@@ -20,6 +25,7 @@ function App()
       <span className="Heading">
         Investing Made Easy.
       </span>
+      <DisplayGraphs data={backendData} />
     </div>
   );
 }
